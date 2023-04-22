963c9ceb3e02c1a2b47be046daa5231d49185e42
Ganishk <ganishk1@gmail.com>
2023-04-22 21:22:16 +0530

    with pca

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import pandas as pd
 
 from preProcess import *
 
-DATASET="try.csv"
+DATASET = "try.csv"
+#DATASET = "BitcoinHeistData.csv"
 
 def split_dataset(data,perc=.80):
     # 80 - 20 % is Pareto principle by default
     global tr,te,vl
-    print("Spliting data...")
+    print("Spliting data...",end="")
 
     n = len(data)
     train_len = round(n*perc*perc)
@@ -19,17 +20,18 @@ def split_dataset(data,perc=.80):
     tr = data[:train_len]
     te = data[train_len:train_len+test_len]
     vl = data[train_len+test_len:]
+
+    te.reset_index(drop=True,inplace=True)
+    vl.reset_index(drop=True,inplace=True)
+    print("Done")
     
 def main():
     df = pd.read_csv(DATASET)
     split_dataset(df)
     del df
     preprocess(tr)
-    print(tr.info())
-    print('\nmean\n%s'%tr.mean())
-    print('\nvariance\n%s'%tr.var())
-    print('\nskewness\n%s'%tr.skew())
-    print('\nkurtosis\n%s'%tr.kurtosis())
+    tr.info()
+    print(tr)
 
 if __name__=="__main__":
     main()
--- preProcess.py
+++ preProcess.py
@@ -1,17 +1,27 @@
 import pandas as pd, numpy as np
 
+def status_decorator(func):
+    def decorate(*params,status=""):
+        print(status,end="")
+        func(*params)
+        print("Done")
+    return decorate
+
+@status_decorator
 def change_dtypes(df):
+    print("Changing datatypes...",end="")
     df.address  = df.address.astype('object')
     df.year     = df.year.astype(np.int64)
     df.day      = df.day.astype(np.int64)
     df.length   = df.length.astype(np.int64)
     df.weight   = df.weight.astype(np.float64)
-    df.counts    = df.counts.astype(np.int64)
+    df.counts   = df.counts.astype(np.int64)
     df.looped   = df.looped.astype(np.int64)
     df.neighbors= df.neighbors.astype(np.int64)
     df.income   = df.income.astype(np.int64)
     df.label    = pd.Categorical(df.label)
 
+@status_decorator
 def clean_data(dataframe):
     """
     Here, we're using median as a central tendency. In a highly skewed dataset
@@ -19,18 +29,41 @@ def clean_data(dataframe):
     Also, it preserves the datatype of that column if it is a type of integer,
     for which using mean might introduce a float value
     """
-    if not dataframe.isnull().values.any():
-        return
-    dataframe.fillna(dataframe.median(),inplace=True)
-    dataframe.dropna(inplace=True)
+    print("Cleaning data...",end="")
+    if dataframe.isnull().values.any():
+        dataframe.fillna(dataframe.median(),inplace=True)
+        dataframe.dropna(inplace=True)
 
-def dim_reduction(dataframe):
-    pass
+@status_decorator
+def dim_reduction(dataframe,dim):
+    print("Reducing dimensions to",dim,end="")
+    global V
+    meanframe = dataframe.mean()
+    stdframe = dataframe.std(ddof=0,numeric_only=True)
+    print(" from",len(meanframe.index),end="\n")
+    features = meanframe.index
+    for feature in features:
+        dataframe['N'+feature] = (dataframe[feature] - meanframe[feature])/stdframe[feature]
+
+    features = "N"+features
+    covariance_matrix = dataframe[features].cov()
+    eig_vals,eig_vecs = np.linalg.eig(covariance_matrix)
+    idx = eig_vals.argsort()[::-1]
+    eig_vals = eig_vals[idx]
+    eig_vecs = eig_vecs[:,idx]
+
+    V = eig_vecs[:,:dim]
+    dataframe = pd.concat([dataframe,dataframe[features]@V],axis=1)
+    print(dataframe)
+    
 
 def preprocess(dataframe):
     
     #column name count, has a name collision with an inbuilt function of pandas
     dataframe.rename(columns={"count":"counts"},inplace=True)
 
+    #clean_data(dataframe,"Cleaning...")
     clean_data(dataframe)
     change_dtypes(dataframe)
+    dim_reduction(dataframe,6)
+
